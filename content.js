804d1230e850e3f491d202463e420df66ef15ab0
Ezra Jesalva <ejesalv1@uncc.edu>
2018-12-22 22:24:50 -0500

    init commit

---
+++ content.js
@@ -0,0 +1,17 @@
+console.log("ready to go?");
+
+// $.ajax({
+//   url: "http://ezrajesalva.pythonanywhere.com/one/",
+//   type: "get", //send it through get method
+//   data: {
+//     ajaxid: 4,
+//     UserID: UserID,
+//     EmailAddress: EmailAddress
+//   },
+//   success: function(response) {
+//     //Do Something
+//   },
+//   error: function(xhr) {
+//     //Do Something to handle error
+//   }
+// });
