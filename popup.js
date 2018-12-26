5e617ef62a57d4ec5311b3ab5ca7ea5f16a70966
Ezra Jesalva <ezra.jesalva15@gmail.com>
2018-12-26 13:15:25 -0500

    clean up js file

--- popup.js
+++ popup.js
@@ -3,35 +3,25 @@ document.addEventListener("DOMContentLoaded", function() {
             url: "http://127.0.0.1:5000/lab-one",
             // The data to send (will be converted to a query string)
             data: {
-                "1": [
-                    "this is the answer for question 1"
-                ],
-                "5": [
-                    "this is the answer for question 5"
-                ],
-                "7": [
-                    "this is the answer for question 7"
-                ]
+              "questions":["1", "5", "7"]
             },
             type: "POST",
             // The type of data we expect back
             dataType: "json",
         })
-        // Code to run if the request succeeds (is done);
-        // The response is passed to the function
+
         .done(function(json) {
-          $("h1").text(json)
+          console.log(json.answers);
+
         })
-        // Code to run if the request fails; the raw request and
-        // status codes are passed to the function
         .fail(function(xhr, status, errorThrown) {
             alert("Sorry, there was a problem!");
+
             console.log("Error: " + errorThrown);
             console.log("Status: " + status);
             console.dir(xhr);
         })
-        // Code to run regardless of success or failure;
         .always(function(xhr, status) {
-            alert("The request is complete!");
+            console.log("fomosjed")
         });
-      });
+});
