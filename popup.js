b8f2ac38ac1d83fbd0eb5e5ea68cff294b065f3e
Ezra Jesalva <ezra.jesalva15@gmail.com>
2018-12-28 22:38:14 -0500

    Enable actual login, use client side validation

--- popup.js
+++ popup.js
@@ -1,28 +1,31 @@
 document.addEventListener("DOMContentLoaded", function() {
     var labNum = $(".lab").val();
-    console.log(labNum);
-    $.ajax({
-            url: "http://127.0.0.1:5000/sign-in",
-            // The data to send (will be converted to a query string)
-            data: JSON.stringify({
-              user: "ezra.jesalva15@gmail.com",
-              password:"Ezra1998"
 
-            }),
-            type: "POST",
-            // The type of data we expect back
-            contentType: "application/json",
-            dataType: "json",
+    $("#submit").click(function() {
+            var user = $("#user").val();
+            var password = $("#password").val();
+            console.log(user);
 
-        })
-        .done(function(json) {
-          console.log(json);
-        })
-        .fail(function(xhr, status, errorThrown) {
-            alert("Sorry, there was a problem!");
+                $.ajax({
+                        url: "http://127.0.0.1:5000/sign-in",
+                        // The data to send (will be converted to a query string)
+                        data: JSON.stringify({
+                            user: user,
+                            password: password
 
-        })
-        .always(function(xhr, status) {
-        });
+                        }),
+                        type: "POST",
+                        // The type of data we expect back
+                        contentType: "application/json",
+                        dataType: "json",
 
-});
+                    })
+                    .done(function(json) {
+                        console.log(json);
+                    })
+                    .fail(function(xhr, status, errorThrown) {
+                        alert("Sorry, there was a problem!");
+                    })
+                    .always(function(xhr, status) {});
+          })
+    });
