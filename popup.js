251b5effd5530ee42aff29f6d4ba8bcaa1351578
Ezra Jesalva <ezra.jesalva15@gmail.com>
2018-12-28 22:14:25 -0500

    Enable login through posted JSON

--- popup.js
+++ popup.js
@@ -1,28 +1,28 @@
 document.addEventListener("DOMContentLoaded", function() {
+    var labNum = $(".lab").val();
+    console.log(labNum);
     $.ajax({
-            url: "http://127.0.0.1:5000/lab-one",
+            url: "http://127.0.0.1:5000/sign-in",
             // The data to send (will be converted to a query string)
-            data: {
-              "questions":["1", "5", "7"]
-            },
+            data: JSON.stringify({
+              user: "ezra.jesalva15@gmail.com",
+              password:"Ezra1998"
+
+            }),
             type: "POST",
             // The type of data we expect back
+            contentType: "application/json",
             dataType: "json",
+
         })
-
         .done(function(json) {
-          console.log(json.answers);
-
+          console.log(json);
         })
         .fail(function(xhr, status, errorThrown) {
             alert("Sorry, there was a problem!");
 
-            console.log("Error: " + errorThrown);
-            console.log("Status: " + status);
-            console.dir(xhr);
         })
         .always(function(xhr, status) {
-            console.log("fomosjed")
         });
 
 });
