2657b82c29e7eac7f2b1a6343532c2c019b7ea43
Ezra Jesalva <ezra.jesalva15@gmail.com>
2019-01-05 08:14:51 -0500

    get lab on load

--- popup.js
+++ popup.js
@@ -1,51 +1,59 @@
+const baseUrl = "http://127.0.0.1:5000/" //for testing
+
 document.addEventListener("DOMContentLoaded", function() {
     checkSignedIn();
-    $("#signInForm").submit(function(e) {
-        e.preventDefault();
-        var user = $("#user").val();
-        var password = $("#password").val();
+    getLab();
 
-        $.ajax({
-                url: baseUrl + "sign-in",
-                data: JSON.stringify({
-                    user: user,
-                    password: password
-                }),
-                type: "POST",
-                // The type of data we expect back
-                contentType: "application/json",
-                dataType: "json",
-            })
-            .done(function(json) {
-              console.log(json)
-                if(json === "YES"){
-                  login();
-                }
-            })
-            .fail(function(xhr, status, errorThrown) {
-            })
-            .always(function(xhr, status) {});
-    })
 });
-var baseUrl = "http://127.0.0.1:5000/" //for testing
+
 $(".lab").on("change",function(e){
-    var labNum = $(this).val();
+  getLab();
+
+});
+
+function getLab(){
+  var labNum = $(".lab").val();
+
+  $.ajax({
+          url: baseUrl + "lab/" + labNum,
+          type: "GET",
+          // The type of data we expect back
+          contentType: "application/json",
+          dataType: "text",
+      })
+      .done(function(json) {
+        console.log(json)
+      })
+      .fail(function(xhr, status, errorThrown) {
+      })
+      .always(function(xhr, status) {console.log('finished lab getting')});
+}
+$("#signInForm").submit(function(e) {
+    e.preventDefault();
+    var user = $("#user").val();
+    var password = $("#password").val();
 
     $.ajax({
-            url: baseUrl + "lab/" + labNum,
-            type: "GET",
+            url: baseUrl + "sign-in",
+            data: JSON.stringify({
+                user: user,
+                password: password
+            }),
+            type: "POST",
             // The type of data we expect back
             contentType: "application/json",
-            dataType: "text",
+            dataType: "json",
         })
         .done(function(json) {
           console.log(json)
+            if(json === "YES"){
+              login();
+            }
         })
         .fail(function(xhr, status, errorThrown) {
         })
-        .always(function(xhr, status) {console.log('finished lab getting')});
-
-});
+        .always(function(xhr, status) {});
+})
 
 function checkSignedIn(){
   console.log('in method')
